5b2ebfea7ec017e62287ca434722149ed1658f78
mraxn <38722959+mraxn@users.noreply.github.com>
2020-12-14 12:25:41 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function GetPhoneNo() {
-  var phoneNoRaw = document.getElementById("phone").value
-  /* var phoneNo = FormatPhoneNumber(phoneNoRaw); */
-  var phoneNo = phoneNoRaw;
-  return phoneNo;
-}
-function OpenLink() {
-  window.open("https://api.whatsapp.com/send?phone=" + GetPhoneNo(), '_blank');
-}
